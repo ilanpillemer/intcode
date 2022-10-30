e267424e481fdc4b142efd0f344112ad041a7a7f
Ilan <ilan.pillemer@gmail.com>
2022-10-30 15:55:55 +0000

    day2

--- d2/main.jl
+++ d2/main.jl
@@ -1,41 +1,68 @@
 # naive beginnings
 println("hello, world!")
-p = Dict()
-open("input", "r") do f
-    s = read(f, String)
-    for (i, x) in enumerate(eachsplit(s, ","))
-        p[i-1] = parse(Int64, x)
+
+function load(name)
+    p = Dict()
+    open(name, "r") do f
+        s = read(f, String)
+        for (i, x) in enumerate(eachsplit(s, ","))
+            p[i-1] = parse(Int64, x)
+        end
     end
+    return p
 end
 
-p[1] = 12
-p[2] = 2
+orig = load("input")
 
-function add(x, y)
+function add(p, x, y)
     a = p[x]
     b = p[y]
     p[a] + p[b]
 end
 
-function mul(x, y)
+function mul(p, x, y)
     a = p[x]
     b = p[y]
     p[a] * p[b]
 end
 
-pc = 0
-opcode = p[pc]
-while opcode != 99
-    x = pc + 1
-    y = pc + 2
-    z = pc + 3
-    a = p[z]
-    if opcode == 1
-        global p[a] = add(x, y)
-    elseif opcode == 2
-        global p[a] = mul(x, y)
+function exec(p)
+    pc = 0
+    opcode = p[pc]
+    while opcode != 99
+        x = pc + 1
+        y = pc + 2
+        z = pc + 3
+        a = p[z]
+        if opcode == 1
+            p[a] = add(p, x, y)
+        elseif opcode == 2
+            p[a] = mul(p, x, y)
+        end
+        pc = pc + 4
+        opcode = p[pc]
     end
-    global pc = pc + 4
-    global opcode = p[pc]
+    p[0]
 end
-println("Position 0 holds value [$(p[0])]")
+
+function run(orig, x, y)
+    p = copy(orig)
+    p[1] = x
+    p[2] = y
+    exec(p)
+end
+
+function run2(orig)
+    for noun = 0:99, verb = 0:99
+        output = (run(orig, noun, verb))
+        if output == 19690720
+            return noun * 100 + verb
+        end
+    end
+end
+
+p1 = run(orig, 12, 2)
+p2 = run2(orig)
+
+println("part 1: $(p1) : $(p1==10566835)") # should be 10566835
+println("part 2: $(p2) : $(p2==2347)") # should be 2347
