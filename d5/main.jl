ac71f7e2afc479f6ef54cf604462314d61be1a50
Ilan <ilan.pillemer@gmail.com>
2022-11-02 11:13:23 +0000

    new api uses channels :tada

--- d5/main.jl
+++ d5/main.jl
@@ -4,15 +4,31 @@ using IntCode
 echo = IntCode.load("echo")
 input = IntCode.load("input")
 
-function run(orig, fn::Function)
+function run(orig, out::Channel, in::Channel)
     p = copy(orig)
-    IntCode.exec(p, fn)
+    IntCode.exec(p, out, in)
 end
 
-accum = run(input, () -> 1)
-println("accum $accum")
 println("part1: final output should be 13787043")
-println(run(input, () -> 5))
+in = Channel(Inf)
+out = Channel(Inf)
+put!(in, 1)
+run(input, out, in)
+
+
+for x in out
+    println("output: $x")
+end
+
 println("part2: final output should be 3892695")
+in = Channel(Inf)
+out = Channel(Inf)
+
+put!(in, 5)
+run(input, out, in)
+
+for x in out
+    println("output: $x")
+end
 
 println("done")
--- d7/main.jl
+++ d7/main.jl
@@ -23,12 +23,34 @@ function part1()
         d = copy(input)
         e = copy(input)
 
-        res = IntCode.exec(a, get_input([0, i]))
-        res = IntCode.exec(b, get_input([res[1], j]))
-        res = IntCode.exec(c, get_input([res[1], k]))
-        res = IntCode.exec(d, get_input([res[1], l]))
-        res = IntCode.exec(e, get_input([res[1], m]))
-        push!(signals, res[1])
+        (a_in, a_out) = (Channel(Inf), Channel(Inf))
+        (b_in, b_out) = (Channel(Inf), Channel(Inf))
+        (c_in, c_out) = (Channel(Inf), Channel(Inf))
+        (d_in, d_out) = (Channel(Inf), Channel(Inf))
+        (e_in, e_out) = (Channel(Inf), Channel(Inf))
+
+        put!(a_in, i)
+        put!(b_in, j)
+        put!(c_in, k)
+        put!(d_in, l)
+        put!(e_in, m)
+
+        put!(a_in, 0)
+        IntCode.exec(a, a_out, a_in)
+
+        put!(b_in, take!(a_out))
+        IntCode.exec(b, b_out, b_in)
+
+        put!(c_in, take!(b_out))
+        IntCode.exec(c, c_out, c_in)
+
+        put!(d_in, take!(c_out))
+        IntCode.exec(d, d_out, d_in)
+
+        put!(e_in, take!(d_out))
+        IntCode.exec(e, e_out, e_in)
+
+        push!(signals, take!(e_out))
     end
     maximum(signals)
 end
